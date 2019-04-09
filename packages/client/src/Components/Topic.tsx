ae14941c8babab1ae795cd3c6e182f2ad34d65d2
Jacob Chang <mrchangji@outlook.com>
2019-04-09 22:57:04 +0800

    remove type icon

--- packages/client/src/Components/Topic.tsx
+++ packages/client/src/Components/Topic.tsx
@@ -6,23 +6,12 @@ export interface ITopicProps {
   topic: ITopic;
 }
 
-const icons = {
-  [TopicType.PUBLIC]: "public",
-  [TopicType.PRIVATE]: "private"
-};
-
 export class Topic extends React.Component<ITopicProps> {
   render() {
     const { topic } = this.props;
     return (
       <div className="topic">
-        <h4 className="topic__title">
-          <img
-            className="topic__type"
-            src={`/images/${icons[topic.type]}.svg`}
-          />
-          {topic.title}
-        </h4>
+        <h4 className="topic__title">{topic.title}</h4>
       </div>
     );
   }
